38b659ff3c12b5136019e7b41a4c2181946ee3d7
FallingCliff <yuluo688@163.com>
2026-09-25 10:59:33 +0900

    chore: release v1.8.20

--- internal/service/service.go
+++ internal/service/service.go
@@ -17,7 +17,7 @@ import (
 const (
 	PluginID      = "credit-manager"
 	PluginName    = "CPA Credit Manager"
-	PluginVersion = "1.8.19"
+	PluginVersion = "1.8.20"
 	// CallerScopeMetadataKey mirrors sdk/cliproxy/executor.CallerScopeMetadataKey.
 	CallerScopeMetadataKey = "caller_scope"
 )
